e940a0800b062dccd04130c67b991f3c0b787f33
Lin Lu <lulin529@gmail.com>
2023-07-30 19:50:53 -0700

    remove home and login page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,5 @@
 import { Routes, Route} from 'react-router-dom';
 
-import Home from '../pages/Home';
-import Signup from '../pages/Signup';
-import Login from '../pages/Login';
 import Question from '../pages/Question';
 import QuestionCreate from '../pages/QuestionCreate';
 import QuestionEdit from '../pages/QuestionEdit';
@@ -11,10 +8,7 @@ const MyRouter = () => {
 
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/sign-up" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/questions" element={<Question />} />
+            <Route path="/" element={<Question />} />
             <Route path="/questions/create" element={<QuestionCreate />} />
             <Route path="/questions/:id/edit" element={<QuestionEdit />} />
         </Routes>
